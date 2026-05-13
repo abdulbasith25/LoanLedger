0eafe13786f9651855f6decaff68165b7a56f76b
abdul basith <abdulbasith2590@gmail.com>
2026-05-13 17:37:42 +0530

    added response entity for resst api return

--- src/main/java/com/loanledger/controller/WalletController.java
+++ src/main/java/com/loanledger/controller/WalletController.java
@@ -5,6 +5,7 @@ import com.loanledger.dto.UserDto;
 import com.loanledger.dto.WalletDepositRequest;
 import com.loanledger.service.WalletService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
@@ -13,21 +14,22 @@ import java.math.BigDecimal;
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class WalletController {
-    private final WalletService walletService;
+    private final WalletService walletService; 
 
     @PostMapping("/deposit")
-    public String deposit(@RequestBody WalletDepositRequest request) {
+    public ResponseEntity<String> deposit(@RequestBody WalletDepositRequest request) {
         walletService.deposit(request.getUserId(), request.getAmount(), "WALLET_DEP");
         return "Deposit successful";
     }
 
     @GetMapping("/{userId}/balance")
-    public BigDecimal getBalance(@PathVariable Long userId) {
-        return walletService.getBalance(userId);
+    public ResponseEntity<BigDecimal> getBalance(@PathVariable Long userId) {
+        BigDecimal balance = walletService.getBalance(userId);
+        return new ResponseEntity<>(balance, HttpStatus.OK);
     }
 
     @PostMapping("/users")
-    public UserDto createUser(@RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserDto> createUser(@RequestBody UserCreateRequest request) {
         return walletService.createUser(request.getName());
     }
 }
