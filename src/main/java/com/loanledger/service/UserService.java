6b473a1f91998f9203c6859ad5ed93c236e9b093
abdul basith <abdulbasith2590@gmail.com>
2026-05-13 11:39:27 +0530

    removed user.save() cause inside @Transactional method hibernate detects the changes in the entity inside a transactional method

--- src/main/java/com/loanledger/service/UserService.java
+++ src/main/java/com/loanledger/service/UserService.java
@@ -2,15 +2,15 @@ package com.loanledger.service;
 
 import com.loanledger.entity.User;
 import com.loanledger.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Transactional
     public void incrementScore(Long userId, int scoreToAdd) {
         User user = userRepository.findById(userId)
@@ -19,7 +19,6 @@ public class UserService {
         int newScore = user.getScore() + scoreToAdd;
         
         user.setScore(newScore);
-        userRepository.save(user);
     }
 
     public Long getUserIdFromUsername(String username) {
